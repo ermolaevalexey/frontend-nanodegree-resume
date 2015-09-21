a68374c0bcfc741a6889835ba8af8a494867f589
ermolaevalexey <descendended@gmail.com>
2015-09-21 20:28:59 +0300

    new resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,16 +20,106 @@ formattedName = HTMLheaderName.replace("%data%", name);
 
 formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedName, formattedRole);
+
+
+//$("#header").prepend(formattedName, formattedRole);
 
 
 /*var str1 = "audacity";
 var str2 = str1.slice(1);
 console.log( str2.replace("u", "U") );*/
 
-var arr = [1,2,3];
+/*var arr = [1,2,3];
 console.log(arr.length);
 
 for (var i = 0; i < arr.length; i++) {
 	console.log( i +" "+arr[i]);
+}*/
+
+var bio = {
+	name: formattedName,
+	role: formattedRole,
+	contacts : {
+		mobile : "+7 (911) 034 62 37",
+		email: "descendended@gmail.com",
+		github: "https://github.com/ermolaevalexey",
+		twitter : "@ermolaevalexey",
+		location: "Saint-Petersburg"
+	},
+	welcomeMessage: "Hello to all!",
+	skills: ["web developing", "HTML", "CSS", "JavaScript", "PHP", "JQuery", "SASS", "basic Java"],
+	biopic : "images/bio.jpg",
+	display : function() {
+		return true;
+	}
+};
+
+$("#header").prepend(bio.name, bio.role);
+
+var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+
+
+for (var prop in bio.contacts){
+
+	//console.log( prop + ':' + bio.contacts[prop]);
+	$("#header #topContacts").append('<li class="flex-item"><span class="orange-text">'+prop+'</span>'+bio.contacts[prop]+'</li>');
+
+}
+
+var welMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+$("#header").append(formattedPic);
+
+$("#header").append(welMsg);
+
+$("#header").append(HTMLskillsStart);
+
+for (var i = 0; i < bio.skills.length; i++) {
+	//console.log(bio.skills[i]);
+	$("#header #skills").append('<li class="flex-item"><span class="white-text">'+bio.skills[i]+"</span></li>");
+}
+
+var work = {
+	jobs : [
+
+		{ 
+			employer   : "Webdynamika (Network Information Technology Co., Ltd)",
+			title	   : "HTML coder/frontend web developer",
+			location   : "Saint-Petersburg",
+			dates 	   : "September 2014 — until now",
+			description: ""
+		},
+
+		{ 
+			employer : "Saint-petersburg University Of The Humanities And Social Sciences",
+			title	 : "Website administrator/web master/website editor",
+			location : "Saint-Petersburg",
+			dates 	 : "March 2013 — June 2014",
+			description: ""
+		}
+
+	]
+
+};
+
+var education = {
+	schools : [
+			{
+			  name: "Saint-petersburg University Of The Humanities And Social Sciences",
+        	  location: "Saint-Petersburg",
+        	  degree: "Higher",
+        	  majors: "Direction of Multimedia Programs",
+        	  dates: 2012,
+        	  url: "http://gup.ru/"
+			}
+	
+		]	 
+}
+
+
+for (var edu in education.schools){
+
+	//console.log( prop + ':' + bio.contacts[prop]);
+	$("#education").append('<div class="education-entry"><a href="#">'+education.schools.name+'</div>');
+
 }
